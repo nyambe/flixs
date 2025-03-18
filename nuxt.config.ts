3b74192459d4741bb859887730ea7ecba277e36f
samiebuka <samuel.ebuka@gmail.com>
2025-03-18 12:32:23 +0100

    vercerl edge

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,9 @@ export default defineNuxtConfig({
   //     autoSubfolderIndex: false
   //   }
   // },
+  nitro: {
+    preset: 'vercel-edge',
+  },
   modules: [
     '@nuxt/ui',
     '@nuxt/fonts',
