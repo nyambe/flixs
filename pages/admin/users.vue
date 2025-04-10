0a92ba8fc114c18138bceee038fc7470198a2034
samiebuka <samuel.ebuka@gmail.com>
2025-04-10 18:01:41 +0200

    Types of subscription.

--- pages/admin/users.vue
+++ pages/admin/users.vue
@@ -108,6 +108,11 @@ const getSubscriptionStatus = (user: User) => {
     : 'Unknown';
 };
 
+// Show user details modal
+const showUserDetails = (user: User) => {
+ console.log(user);
+};
+
 // Load users on component mount
 onMounted(() => {
   fetchUsers();
@@ -132,7 +137,7 @@ onMounted(() => {
             type="text"
             placeholder="Search users..."
             class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
+          >
           <span class="absolute right-3 top-2 text-gray-400">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
@@ -221,7 +226,7 @@ onMounted(() => {
                 >
                   View in Stripe
                 </a>
-                <button class="text-gray-600 hover:text-gray-900">
+                <button @click="showUserDetails(user)" class="text-gray-600 hover:text-gray-900">
                   Details
                 </button>
               </td>
